f83ca5151d27b29ce2329af5eac49fe18ff22acf
thomasBelray <thomas.sepulveda.belray@gmail.com>
2021-11-03 00:38:10 -0300

    first commit

---
+++ src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <h1>Hola mundo</h1>
+      <h1>Hola mundo</h1>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
