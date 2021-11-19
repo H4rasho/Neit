3038cdc3e248ea0086df840183ef7cd1445672a4
paolo urbina acuña <75394694+PUAzuzuINF@users.noreply.github.com>
2021-11-19 00:37:58 -0300

    Navbar y home
    - Se agregaron link en el navbar para cambiar de vista.
    - Se cambió el diseño del home.
    - Queda pendiente la vista de inscripción.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,64 @@
 import React from "react";
+import "./styles/Home.css"
 
 export const Home = () => {
   return (
     <div>
-      <h1>HOLA HOME</h1>
+          <div><br></br></div>
+          <div class="alert alert-info cajita" role="alert">
+            ¡Bienvenid@ al sistema de inscripción!
+          </div>
+
+          <div class="accordion cajita" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                ¿Qué es el sistema de inscripción?
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                <strong>El sistema de inscripción UTEM</strong> es una de las plataformas disponibles por la universidad para que l@s estudiantes puedan inscribir sus asignaturas cuando corresponda.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Inscripción de asignaturas
+                </button>
+              </h2>
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Mi malla curricular
+                </button>
+              </h2>
+              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Mi malla curricular
+                </button>
+              </h2>
+              <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                <strong>El sistema de inscripción UTEM</strong> es una de las plataformas disponibles por la universidad para que l@s estudiantes puedan inscribir sus asignaturas cuando corresponda.
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
   );
 };
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,9 +1,39 @@
 import React from "react";
+import "./styles/Navbar.css";
 
 export const Navbar = () => {
   return (
     <div>
-      <h1>NavBar</h1>
+          <div className="divUno">
+            <h1>Sistema de inscripción UTEM</h1>
+          </div>
+        
+          <nav className="navbar navbar-expand-lg navbar-dark navm">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link active" href="#">Inscripción de ramos</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link active" href="#">Mi horario</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link active" href="#"> Mi malla curricular</a>
+                    </li>
+                    <li className="nav-item out">
+                    <a className="nav-link active" href="#">Cerrar sesión</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+          </nav>
     </div>
   );
 };
