4a64f2b538d3cba8837f4653843904ed81d33b55
thomasBelray <thomas.sepulveda.belray@gmail.com>
2021-11-25 20:05:40 -0300

    obeter asignturas por tomar add

---
+++ src/actions/asignaturas.js
@@ -0,0 +1,23 @@
+import { fetchConToken } from "../helpers/fetch";
+import { types } from "../types/types";
+
+export const startGetAsignaturas = (idEstudiante, idCarrera) => {
+  return async (dispatch) => {
+    const resp = await fetchConToken(
+      `asignaturas?idEstudiante=${idEstudiante}&idCarrera=${idCarrera}`
+    );
+    const body = await resp.json();
+    console.log(idCarrera);
+
+    if (body.ok) {
+      dispatch(getAsignaturas(body.ramos));
+    } else {
+      console.log("err", body.msg);
+    }
+  };
+};
+
+const getAsignaturas = (asginatruas) => ({
+  type: types.inscripcionGetAsginaturas,
+  payload: asginatruas,
+});
