7bb93fc28aa81cbf65afc3ea9c3ca111fb9a30b5
thomasBelray <thomas.sepulveda.belray@gmail.com>
2021-11-30 01:05:43 -0300

    react calendar

--- src/components/horario/Horario.js
+++ src/components/horario/Horario.js
@@ -1,154 +1,35 @@
-import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const myEventsList = [];
 
 export const Horario = () => {
   return (
     <div>
-      <div>
-        <div className="row m-1">
-          <div className="col-8 columna2">
-            <table className="table table-bordered table-striped border-light text-center Thorario">
-              <thead>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    Periodo
-                  </th>
-                  <th scope="row" colSpan="2">
-                    Bloque
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Lunes
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Martes
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Miércoles
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Jueves
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Viernes
-                  </th>
-                  <th scope="col" colSpan="2">
-                    Sábado
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    1
-                  </th>
-                  <td colSpan="2">8:00 - 9:30</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    2
-                  </th>
-                  <td colSpan="2">9:40 - 11:10</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    3
-                  </th>
-                  <td colSpan="2">11:20 - 12:50</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    4
-                  </th>
-                  <td colSpan="2">13:00 - 14:30</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    5
-                  </th>
-                  <td colSpan="2">14:40 - 16:10</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    6
-                  </th>
-                  <td colSpan="2">16:20 - 17:50</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    7
-                  </th>
-                  <td colSpan="2">18:00 - 19:30</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    8
-                  </th>
-                  <td colSpan="2">19:40 - 21:10</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-                <tr>
-                  <th scope="row" colSpan="2">
-                    9
-                  </th>
-                  <td colSpan="2">21:20 - 22:50</td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                  <td colSpan="2"></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        view="week"
+        style={{ height: 1000 }}
+      />
     </div>
   );
 };
