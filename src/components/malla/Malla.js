3811bba05c1e9b798b0ab1c012867e2e27432b24
Kleim <nickonh.arenas@gmail.com>
2021-12-17 18:14:59 -0300

    Malla
    Se incorporan el modulo de malla, ademas de la leyenda

--- src/components/malla/Malla.js
+++ src/components/malla/Malla.js
@@ -3,7 +3,25 @@ import React from "react";
 export const Malla = () => {
   return (
     <div>
-      <h2>No disponible</h2>
+      <div className="m-2">
+        <h2>Malla</h2>
+        <div className="row m-1 border border-2 border-dark bg-light">
+            <div className="m-1">
+              <table className="table table-condensed" width="100%">
+                <tbody>
+                  <tr>
+                    <td className="table-success text-center" width="20%">Aprobado</td>
+                    <td className="table-primary text-center legend" width="20%">Inscrito</td>
+                    <td className="table-danger text-center legend" width="20%">Reprobado</td>
+                    <td className="table-secondary text-center legend" width="20%">No Cursado</td>
+                    <td className="table-warning text-center legend" width="20%">Requisito de Asignatura</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          <div className="m-1 border border-2 border-dark">Semestres</div>
+        </div>
+      </div>
     </div>
   );
 };
