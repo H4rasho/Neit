6dae99a20ad644b607fb68b43de441a2d58b13e8
thomasBelray <thomas.sepulveda.belray@gmail.com>
2021-12-23 11:08:17 -0300

    colores malla add

--- src/components/malla/Semestres.js
+++ src/components/malla/Semestres.js
@@ -22,80 +22,81 @@ export const Semestres = () => {
           {/*CUADRO DEL NÚMERO DE SEMESTRE*/}
           <div className="overflow-auto">
             <div className="flex">
-                {getHeadMalla(malla.asignaturas).map((numeroSemestre) => (
-                  <div key={numeroSemestre} >
-                    <h4 className="flex-element">{numeroSemestre}</h4>
-                  </div>
-                ))}
+              {getHeadMalla(malla.asignaturas).map((numeroSemestre) => (
+                <div key={numeroSemestre}>
+                  <h4 className="flex-element">{numeroSemestre}</h4>
+                </div>
+              ))}
             </div>
-            
+
             {/*BOTONES DE LAS ASIGNATURAS*/}
             <div className="flex">
-                {getHeadMalla(malla.asignaturas).map((h) => (
-                  <section key={h}>
-                    {getRows(malla.asignaturas, h).map((asignatura) => (
-                      <div key={asignatura.id._id} className="m-1">
-                        <button
-                          type="button"
-                          className="btn btn-primary botonAsignatura"
-                          data-bs-toggle="modal"
-                          data-bs-target="#staticBackdrop"
-                        >
-                          <div className="text-end">{asignatura.promedio}</div> {/*NOTA ASIGNATURA*/}
-                          <p>{asignatura.id.nombre}</p>
-                        </button>
-                        <div
-                          className="modal fade"
-                          id="staticBackdrop"
-                          data-bs-backdrop="static"
-                          data-bs-keyboard="false"
-                          tabIndex="-1"
-                          aria-labelledby="staticBackdropLabel"
-                          aria-hidden="true"
-                        >
-                          <div className="modal-dialog">
-                            <div className="modal-content">
-                              <div className="modal-header">
-                                <h5
-                                  className="modal-title"
-                                  id="staticBackdropLabel"
-                                >
-                                  Detalles Asignatura
-                                </h5>
-                                <button
-                                  type="button"
-                                  className="btn-close"
-                                  data-bs-dismiss="modal"
-                                  aria-label="Close"
-                                ></button>
+              {getHeadMalla(malla.asignaturas).map((h) => (
+                <section key={h}>
+                  {getRows(malla.asignaturas, h).map((asignatura) => (
+                    <div key={asignatura.id._id} className="m-1">
+                      <button
+                        type="button"
+                        className={`btn btn-primary botonAsignatura ${asignatura.estado}`}
+                        data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"
+                      >
+                        <div className="text-end">{asignatura.promedio}</div>{" "}
+                        {/*NOTA ASIGNATURA*/}
+                        <p>{asignatura.id.nombre}</p>
+                      </button>
+                      <div
+                        className="modal fade"
+                        id="staticBackdrop"
+                        data-bs-backdrop="static"
+                        data-bs-keyboard="false"
+                        tabIndex="-1"
+                        aria-labelledby="staticBackdropLabel"
+                        aria-hidden="true"
+                      >
+                        <div className="modal-dialog">
+                          <div className="modal-content">
+                            <div className="modal-header">
+                              <h5
+                                className="modal-title"
+                                id="staticBackdropLabel"
+                              >
+                                Detalles Asignatura
+                              </h5>
+                              <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                              ></button>
+                            </div>
+                            <div className="modal-body">
+                              <div className="card">
+                                <ul className="list-group list-group-flush">
+                                  <li className="list-group-item">Estado : </li>
+                                  <li className="list-group-item">Tipo : </li>
+                                  <li className="list-group-item">Nota : </li>
+                                </ul>
                               </div>
-                              <div className="modal-body">
-                                <div className="card">
-                                  <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Estado : </li>
-                                    <li className="list-group-item">Tipo : </li>
-                                    <li className="list-group-item">Nota : </li>
-                                  </ul>
-                                </div>
-                              </div>
-                              <div className="modal-footer">
-                                <button
-                                  type="button"
-                                  className="btn btn-success"
-                                  data-bs-dismiss="modal"
-                                >
-                                  Aceptar
-                                </button>
-                              </div>
+                            </div>
+                            <div className="modal-footer">
+                              <button
+                                type="button"
+                                className="btn btn-success"
+                                data-bs-dismiss="modal"
+                              >
+                                Aceptar
+                              </button>
                             </div>
                           </div>
                         </div>
                       </div>
-                    ))}
-                  </section>
-                ))}
-              </div>
-            </div>         
+                    </div>
+                  ))}
+                </section>
+              ))}
+            </div>
+          </div>
         </>
       ) : (
         <h1>NO malla</h1>
