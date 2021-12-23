f6dae793cd369fde91cd64a0e5a5aa320d54794d
paolo urbina acuña <75394694+PUAzuzuINF@users.noreply.github.com>
2021-12-23 20:47:48 -0300

    edit Rehacer inscripción
    - Se ha modificado la vista donde el estudiante puede volver a hacer la inscripción de ramos.

--- src/components/inscripcion/InscripcionRealizada.js
+++ src/components/inscripcion/InscripcionRealizada.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { reahcer } from "../../actions/asignaturas";
 
+import "./styles/inscripcion.css";
+
 export const InscripcionRealizada = ({ inscripcion }) => {
   const { name, uid } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -11,15 +13,20 @@ export const InscripcionRealizada = ({ inscripcion }) => {
   };
 
   return (
-    <div>
-      <h1>Estiamdo estudiante {name} sus asignaturas incritas son: </h1>
-      {inscripcion.asignaturas.map((asig) => (
-        <div key={asig.id._id}>
-          <h1>{asig.id.nombre}</h1>
-          <span>profesor: {asig.seccion.docente}</span>
-        </div>
-      ))}
-      <button onClick={handleReHacer}>Rehacer</button>
+    <div className="bg-light container-fluid">
+          <div>
+            <h4>Estimado {name}, </h4>
+            <h5>Las asignaturas que has inscrito son: </h5>
+          </div> 
+        {inscripcion.asignaturas.map((asig) => (
+          <div className="border border-dark border-3 m-2" key={asig.id._id}>
+            <h6 className="border-bottom border-dark border-3 colorCA" >
+              Asignatura: {asig.id.nombre}
+            </h6>
+            <h6>Profesor(a): {asig.seccion.docente}</h6>
+          </div>
+        ))}
+        <button className="btn btn-primary m-3" onClick={handleReHacer}>Rehacer inscripción</button>          
     </div>
   );
 };
